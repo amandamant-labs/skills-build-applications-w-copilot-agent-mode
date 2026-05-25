f58fdf4909d4455a78bac72134b76c370433f042
amandamant-labs <amanda.mantovani@luizalabs.com>
2026-05-25 23:40:55 +0000

    Revert "Atualiza frontend"
    This reverts commit 94c6359843bb521130acb93eb4d0343b5a43afe0.

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -5,7 +5,6 @@ import Leaderboard from './components/Leaderboard';
 import Teams from './components/Teams';
 import Users from './components/Users';
 import Workouts from './components/Workouts';
-import logo from './assets/octofitapp-small.svg';
 import './App.css';
 
 function Home() {
@@ -29,14 +28,13 @@ function App() {
 
   return (
     <div className="App container py-4">
-      <nav className="navbar navbar-expand-lg navbar-custom rounded shadow-sm mb-4">
+      <nav className="navbar navbar-expand-lg navbar-light bg-white rounded shadow-sm mb-4">
         <div className="container-fluid">
-          <NavLink className="navbar-brand d-flex align-items-center fw-bold text-white" to="/">
-            <img src={logo} alt="OctoFit logo" className="app-logo me-2" />
-            <span>OctoFit Tracker</span>
+          <NavLink className="navbar-brand fw-bold" to="/">
+            OctoFit Tracker
           </NavLink>
           <button
-            className="navbar-toggler border-0"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
